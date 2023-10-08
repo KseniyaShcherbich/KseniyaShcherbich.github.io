f2997dfe17f20f71be8a9bba3efb2ab8731461bc
Kseniya <90932596+KseniyaShcherbich@users.noreply.github.com>
2023-10-09 02:40:21 +0300

    Update main.js

--- bwin-page/js/main.js
+++ bwin-page/js/main.js
@@ -9,10 +9,8 @@ for (let i = 0; i < showInfoButtons.length; i++) {
     showInfoButtons[i].style.display = "none";
     hideInfoButtons[i].style.display = "flex";
 
-    const bwinSection = findBwinSection(showInfoButtons[i]);
-
-    if (bwinSection) {
-      bwinSection.style.borderRadius = "15px 15px 0px 0px";
+    if (bwinSections[i]) {
+      bwinSections[i].style.borderRadius = "15px 15px 0px 0px";
     }
   });
 }
@@ -23,20 +21,10 @@ for (let i = 0; i < hideInfoButtons.length; i++) {
     showInfoButtons[i].style.display = "flex";
     hideInfoButtons[i].style.display = "none";
 
-    const bwinSection = findBwinSection(showInfoButtons[i]);
-
-    if (bwinSection) {
-      bwinSection.style.borderRadius = "";
+    if (bwinSections[i]) {
+      bwinSections[i].style.borderRadius = "15px";
     }
   });
-}
-
-function findBwinSection(button) {
-  const infoBlock = button.parentElement;
-
-  const bwinSection = infoBlock.querySelector(".bwin-section");
-
-  return bwinSection;
 }
 
 const block1 = document.getElementById("block1");
